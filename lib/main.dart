8c1cf6cb2444aecb8bcf9e51816735af291e786c
udaykumar-m <udaykumardevil3830@gmail.com>
2024-06-02 22:58:58 +0530

    added listview at top for favourited items

--- lib/main.dart
+++ lib/main.dart
@@ -46,35 +46,36 @@ class MyAppState extends ChangeNotifier {
     notifyListeners();
   }
 
-  void removeFavorite(value){
+  void removeFavorite(value) {
     favorites.remove(value);
     notifyListeners();
   }
 
-  void showToast(BuildContext context, WordPair value){
-    ScaffoldMessenger.of(context).showSnackBar( SnackBar(
+  void showToast(BuildContext context, WordPair value) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
       content: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           RichText(
             text: TextSpan(
-              text:"Removed ",
-              children: <TextSpan>[ 
-                TextSpan(text: value.toString(), style: TextStyle(fontWeight: FontWeight.bold) ),
-               TextSpan(text:" from favourites"),
+              text: "Removed ",
+              children: <TextSpan>[
+                TextSpan(
+                    text: value.toString(),
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+                TextSpan(text: " from favourites"),
               ],
             ),
           ),
         ],
       ),
-      shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(20.0) ),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
       behavior: SnackBarBehavior.floating,
       // width: 260.0,
       duration: const Duration(milliseconds: 800),
       elevation: 0,
     ));
   }
-
 }
 
 class MyHomePage extends StatefulWidget {
@@ -83,42 +84,36 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   var selectedIndex = 0;
   Widget? page;
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     changePages(0);
   }
 
-  void changePages(int selectedIndex){
-    
+  void changePages(int selectedIndex) {
     switch (selectedIndex) {
       case 0:
         setState(() {
           page = GeneratorPage();
         });
-        break;
       case 1:
         setState(() {
           page = FavoritesPage();
         });
         print('FavoritesPage');
-        break;
       default:
-        // setState(() {
-        //   page = GeneratorPage();
-        // });
-        // throw UnimplementedError('no widget for $selectedIndex');
+      // setState(() {
+      //   page = GeneratorPage();
+      // });
+      // throw UnimplementedError('no widget for $selectedIndex');
     }
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.primaryContainer,
       body: page,
@@ -126,7 +121,7 @@ class _MyHomePageState extends State<MyHomePage> {
         destinations: [
           NavigationDestination(
             icon: Icon(Icons.home),
-            label:'Home',
+            label: 'Home',
           ),
           NavigationDestination(
             icon: Icon(Icons.favorite),
@@ -146,7 +141,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class GeneratorPage  extends StatelessWidget {
+class GeneratorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
@@ -158,29 +153,64 @@ class GeneratorPage  extends StatelessWidget {
     } else {
       icon = Icons.favorite_border;
     }
+    var favList = appState.favorites.toList();
 
     return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          // ListView.builder(
+          //     itemCount: appState.favorites.length,
+          //     itemBuilder: (context, i) =>
+          //         Text(appState.favorites[i].toString())),
+
+          Container(
+            height: 250,
+            child: favList.isEmpty
+                ? Center(child: Text('Tap on Like to Add Favorites'))
+                : ListView.builder(
+                    itemCount: favList.length > 4 ? 4 : favList.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      int itemIndex = favList.length > 4
+                          ? (favList.length - 4 + index)
+                          : index;
+
+                      return ListTile(
+                        title: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.favorite),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(favList[itemIndex].toString()),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+          ),
+
           BigCard(pair: pair),
           SizedBox(height: 10),
           Row(
             mainAxisSize: MainAxisSize.min,
             children: [
-
               ElevatedButton.icon(
                 onPressed: () {
                   appState.toggleFavorite();
                 },
-                icon: Icon(icon,size: 20.0,),
+                icon: Icon(
+                  icon,
+                  size: 20.0,
+                ),
                 label: Text('Like'),
               ),
               SizedBox(width: 10),
-
               ElevatedButton(
                 onPressed: () {
                   appState.getNext();
+                  print(favList.isEmpty);
                 },
                 child: Row(
                   children: [
@@ -208,7 +238,6 @@ class BigCard extends StatelessWidget {
 
   final WordPair pair;
 
-
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
@@ -217,10 +246,7 @@ class BigCard extends StatelessWidget {
       color: theme.colorScheme.onPrimary,
     );
     final styleWord = theme.textTheme.displayMedium!.copyWith(
-      color: theme.colorScheme.onPrimary,
-      fontWeight: FontWeight.bold
-    );
-
+        color: theme.colorScheme.onPrimary, fontWeight: FontWeight.bold);
 
     return Card(
       color: theme.colorScheme.primary,
@@ -229,39 +255,42 @@ class BigCard extends StatelessWidget {
         child: Wrap(
           children: [
             Text(pair.first, style: style),
-            Text(pair.second, style: styleWord,),
+            Text(
+              pair.second,
+              style: styleWord,
+            ),
           ],
         ),
       ),
     );
   }
 }
+
 class FavoritesPage extends StatelessWidget {
   const FavoritesPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-
     final theme = Theme.of(context);
 
     var appState = context.watch<MyAppState>();
-    var messages =  <WordPair>[];
+    var messages = <WordPair>[];
     if (appState.favorites.isEmpty) {
       return Center(
         child: Text('No favorites yet.'),
       );
-    }
-    else{
+    } else {
       messages = appState.favorites;
     }
 
     return SafeArea(
       child: Padding(
-        padding: const EdgeInsets.only(left:20.0, top: 20.0),
+        padding: const EdgeInsets.only(left: 20.0, top: 20.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text("you have ${messages.length} favourites :", style: const TextStyle(fontWeight: FontWeight.bold)),
+            Text("you have ${messages.length} favourites :",
+                style: const TextStyle(fontWeight: FontWeight.bold)),
             SizedBox(height: 10),
             Expanded(
               child: SingleChildScrollView(
@@ -292,7 +321,11 @@ class FavouritesGrid extends StatelessWidget {
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     return GridView.builder(
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, childAspectRatio: 3, crossAxisSpacing: 2.0, mainAxisSpacing: 2.0),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3,
+          crossAxisSpacing: 2.0,
+          mainAxisSpacing: 2.0),
       // padding: EdgeInsets.zero,
       shrinkWrap: true,
       itemCount: messages.length,
@@ -300,26 +333,33 @@ class FavouritesGrid extends StatelessWidget {
       itemBuilder: (context, index) {
         return Container(
           margin: EdgeInsets.all(5),
-          decoration: BoxDecoration(border: Border.all(color: theme.colorScheme.primary, width: 2),borderRadius: BorderRadius.all(Radius.circular(25))),
+          decoration: BoxDecoration(
+              border: Border.all(color: theme.colorScheme.primary, width: 2),
+              borderRadius: BorderRadius.all(Radius.circular(25))),
           padding: const EdgeInsets.only(left: 8),
           child: Row(
             mainAxisSize: MainAxisSize.min,
-              children: [
-                IconButton(
-                  icon: Icon(
-                    Icons.delete_outline,
-                    size: 24.0,
-                    color: theme.colorScheme.primary,
-                    shadows: <Shadow>[Shadow(color: Colors.white , blurRadius: 10.0)],
-                  ),
-                  onPressed: () =>{ 
-                    appState.showToast(context, messages[index]),
-                    appState.removeFavorite(messages[index]),
-                  },
+            children: [
+              IconButton(
+                icon: Icon(
+                  Icons.delete_outline,
+                  size: 24.0,
+                  color: theme.colorScheme.primary,
+                  shadows: <Shadow>[
+                    Shadow(color: Colors.white, blurRadius: 10.0)
+                  ],
                 ),
-                Text(messages[index].toString(), style: TextStyle(fontSize: 16),),
-              ],
-            ),
+                onPressed: () => {
+                  appState.showToast(context, messages[index]),
+                  appState.removeFavorite(messages[index]),
+                },
+              ),
+              Text(
+                messages[index].toString(),
+                style: TextStyle(fontSize: 16),
+              ),
+            ],
+          ),
         );
       },
     );
